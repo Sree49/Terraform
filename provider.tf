1ec79f2d3e799622863e4eed93c5ab7a8717a16f
Vijaya Koneru <vijayalaxmi4363@gmail.com>
2026-09-24 03:45:47 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -7,12 +7,23 @@ terraform {
   }
 }
 
+module "Keyvault" {
+  source      = "./modules/keyvault"
+  RG_Name     = var.RG_Name
+  RG_Location = var.RG_Location
+}
+
 provider "azurerm" {
   # Configuration options
+  subscription_id = module.Keyvault.subscription-id
+  tenant_id       = module.Keyvault.tenant-id
+  client_id       = module.Keyvault.client-id
+  client_secret   = module.Keyvault.client-secret
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
+
 
   }
 }
